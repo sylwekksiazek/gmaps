f5ab71f6db85edc778bb4eedb46e60437182bf06
sylwekksiazek <sylwek.ksiazek@gmail.com>
2013-01-18 14:02:54 +0100

    Update gmaps.py
    update

--- gmaps.py
+++ gmaps.py
@@ -2,27 +2,20 @@ from googlemaps import GoogleMaps
 import re
 gmaps = GoogleMaps()
 def remove_html_tags(data):
-    p = re.compile(r'<.*?>')
+	p = re.compile(r'<.*?>')
 	return p.sub('', data)
 print "Wybierz odpowiednia opcje."
 print "1. Pokaz trase"
 print "2. Pokaz w poblizu..."
+print "3. Pokaz trase do sprecyzowanego celu."
 wybor = input()
 cel = ""
 if wybor == 1:
-	address1 = raw_input('Podaj nazwe miejscowosci startowej: ')
-	address2 = raw_input('Podaj nazwe miejscowosci koncowej: ')
-	print ""
-	lat1, lng1 = gmaps.address_to_latlng(address1)
-	lat2, lng2 = gmaps.address_to_latlng(address2)
-	check = ""
-	#print lat1, lng1
-	#print lat2, lng2
 	try:
-		destination1 = gmaps.latlng_to_address(lat1, lng1)
-		destination2 = gmaps.latlng_to_address(lat2, lng2)
-		#print destination1
-		#print destination2
+		address1 = raw_input('Podaj nazwe miejscowosci startowej: ')
+		address2 = raw_input('Podaj nazwe miejscowosci koncowej: ')
+		print ""
+		check = ""
 		directions = gmaps.directions(address1,address2)
 		print "Dystans: ",
 		print float(directions['Directions']['Distance']['meters'])/1000, 'kilometrow'
@@ -35,9 +28,35 @@ if wybor == 1:
 	except ValueError:
 	    print "Wystapil nieznany blad! Przepraszamy."
 elif wybor == 2:
-	cel = raw_input('Czego poszukujesz?: ')
-	address3 = raw_input('W jakiej okolicy?: ')
-	local = gmaps.local_search(cel + ' ' + address3)
-	print local['responseData']['results'][0]['titleNoFormatting']
+	try:
+		cel1 = raw_input('Czego poszukujesz?: ')
+		address3 = raw_input('W jakiej okolicy?: ')
+		lat3, lng3 = gmaps.address_to_latlng(cel1 + ' ' + address3)
+		destination3 = gmaps.latlng_to_address(lat3, lng3)
+		local = gmaps.local_search(cel1 + ' ' + address3)
+		print local['responseData']['results'][0]['titleNoFormatting']
+		print destination3
+	except ValueError:
+		print "Wystapil nieznany blad! Przepraszamy."
+elif wybor == 3:
+	try:
+		address4 = raw_input('Podaj nazwe miejscowosci startowej: ')
+		cel2 = raw_input('Czego poszukujesz?: ')
+		address5 = raw_input('Podaj miejscowosc, w ktorej szukac: ')
+		lat5, lng5 = gmaps.address_to_latlng(cel2 + ' ' + address5)
+		destination5 = gmaps.latlng_to_address(lat5, lng5)
+		local = gmaps.local_search(cel2 + ' ' + address5)
+		directions = gmaps.directions(address4,cel2 + ' ' + address5)
+		print "prowadze do: " + local['responseData']['results'][0]['titleNoFormatting'],
+		print destination5
+		print ""
+		print "Trasa: "
+		for steps in directions['Directions']['Routes'][0]['Steps']:
+			check2 = steps['descriptionHtml']
+			check2 = remove_html_tags(check2)
+			print check2
+	except ValueError:
+	    print "Wystapil nieznany blad! Przepraszamy."
+
 else:
 	print "Zly wybor"
